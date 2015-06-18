5b5fc20b0a6d88f7cc5d442d336dacf1aa12c9ab
허정아 <bit@bit-PC>
2015-06-18 17:01:14 +0900

    doc-board.sql table create

---
+++ web01/doc/board.sql
@@ -0,0 +1,24 @@
+CREATE TABLE board (
+  bno int(11) NOT NULL AUTO_INCREMENT,
+  title varchar(255) NOT NULL,
+  content mediumtext NOT NULL,
+  cre_dt datetime DEFAULT NULL,
+  views int(11) DEFAULT '0',
+  PRIMARY KEY (bno)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+insert into board(title, content) values('aaaa', 'aaaaaaaaa');
+insert into board(title, content) values('bbbbb', 'bbbbbbbbb');
+insert into board(title, content) values('ccccc', 'ddcccccccc');
+insert into board(title, content) values('dddddd', 'ddddddddd');
+insert into board(title, content) values('eeeeee', 'eeeeeeee');
+
+update board set
+  cre_dt=now()
+where bno > 0;
+
+
+
+
+
+
---
+++ web01/step03/src/net/bitacademy/spring/servlet/BoardAddServlet.java
@@ -0,0 +1,54 @@
+package net.bitacademy.spring.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.bitacademy.spring.dao.BoardDao;
+import net.bitacademy.spring.vo.Board;
+
+@WebServlet("/board/add.do")
+public class BoardAddServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+  
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    
+    req.setCharacterEncoding("UTF-8"); // getParameter()를 호출하기 전에 지정해야 한다.
+    
+    Board board = new Board();
+    board.setTitle(req.getParameter("title"));
+    board.setContent(req.getParameter("content"));
+    
+    resp.setContentType("text/html;charset=UTF-8");
+    try {
+      BoardDao boardDao = new BoardDao();
+      boardDao.insert(board);
+      
+      resp.sendRedirect("list.do");
+      return;
+      
+    } catch (Exception e) {
+      req.setAttribute("error", e);
+      RequestDispatcher rd = req.getRequestDispatcher("/error.jsp");
+      rd.include(req, resp);
+
+    } 
+  }
+
+}
+
+
+
+
+
+
+
+
+
